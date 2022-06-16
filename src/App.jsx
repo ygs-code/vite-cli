ab4b313d157b1cb412b914eec92cfe12a402b864
姚观寿 <yaoguanshou@rainbowcn.com>
2022-06-16 19:05:25 +0800

    添加插件

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,43 @@
-import { useState } from 'react';
-import Home from './pages/Home';
-import logo from './logo.svg';
-import './App.css';
-// import demo from './demo.graphql';
+/*
+ * @Date: 2022-05-09 09:42:27
+ * @Author: Yao guan shou
+ * @LastEditors: Yao guan shou
+ * @LastEditTime: 2022-06-16 19:02:44
+ * @FilePath: /vite-cli/src/App.jsx
+ * @Description:
+ */
+import { useState } from 'react'
+import viteLogo from './favicon.svg'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-// if (import.meta) {
-//     console.log(
-//         'import.meta',
-//         import.meta
-//     );
-//     console.log(
-//         'import.meta.env.VITE_APP_BASEAPI=',
-//         import.meta.env.VITE_APP_BASEAPI
-//     );
-//     // console.log(
-//     //     '  import.meta.env.VITE_SOME_KEY==',
-//     //     import.meta.env.VITE_APP_BASEAPI
-//     // );
-// }
-// asdsdf
+function App() {
+  const [count, setCount] = useState(0)
 
-// 这样导出刷新会有问题
-// export default () => {
-const App = () => {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello Vite + React!</p>
-                <p>
-                    <button
-                        type="button"
-                        onClick={() => setCount((count) => count + 1)}
-                    >
-                        count is: {count}
-                    </button>
-                </p>
-                <p>
-                    Edit <code>App.jsx</code> and save to test HMR updates.
-                </p>
-                <p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    {' | '}
-                    <a
-                        className="App-link"
-                        href="https://vitejs.dev/guide/features.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Vite Docs
-                    </a>
-                </p>
-            </header>
-            <Home />
-        </div>
-    );
-};
-
-export default App;
-
-if (import.meta.hot) {
-    import.meta.hot.accept((newModule) => {
-        console.log('代码更新成功');
-        // 这种更新新会刷新浏览器
-        window.location.reload();
-    });
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
+
+export default App
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,12 @@
-import { useState } from 'react';
- 
-
-export default ()=>{
-    return <div>HOME</div>
+var _jsxFileName = "/Users/admin/Documents/code/vite-cli/src/pages/Home/index.jsx";
+import {useState} from "react";
+import {jsxDEV as _jsxDEV} from "react/jsx-dev-runtime";
+export default () => {
+  return /* @__PURE__ */ _jsxDEV("div", {
+    children: "HOME"
+  }, void 0, false, {
+    fileName: _jsxFileName,
+    lineNumber: 5,
+    columnNumber: 12
+  }, this);
 };
